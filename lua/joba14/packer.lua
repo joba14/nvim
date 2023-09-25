d0287fab0e049cd36267351f2c1fdf93378aa924
joba14 <57039383+joba14@users.noreply.github.com>
2023-09-25 09:00:19 +0300

    updated packer.lua to use the latest telescope plugin
    updated packer.lua to use the latest telescope plugin, the previous version had bugs and mainly did not display search results while hot-typing

--- lua/joba14/packer.lua
+++ lua/joba14/packer.lua
@@ -39,7 +39,7 @@ return packer.startup(function(use)
 
 	use({
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.0', -- or: branch = '0.1.x',
+		tag = '0.1.3', -- or: branch = '0.1.x',
 		requires = { { 'nvim-lua/plenary.nvim' } }
 	})
 
